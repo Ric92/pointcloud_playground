d225fde0371c1ae68258dddf9057013a761c4903
ric92 <ricloplop@gmail.com>
2019-11-01 01:41:13 +0100

    Added bbox example

--- examples/example1/main.cpp
+++ examples/example1/main.cpp
@@ -1,8 +1,6 @@
 //---------------------------------------------------------------------------------------------------------------------
 //  POINTCLOUD_PLAYGROUND
 //---------------------------------------------------------------------------------------------------------------------
-//  mico
-//---------------------------------------------------------------------------------------------------------------------
 //  Copyright 2019 ViGUS University of Seville
 //---------------------------------------------------------------------------------------------------------------------
 //  Permission is hereby granted, free of charge, to any person obtaining a copy of this software
@@ -25,34 +23,89 @@
 #include <string>
 
 #include <pointcloud_playground/Visualizer.h>
+
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
+#include <pcl/common/pca.h>
+#include <pcl/common/centroid.h>
+#include <pcl/common/common.h>
+
+typedef pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZRGB> ColorHandlerXYZ;
 
 int main(int argc, char const *argv[])
 {
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
-    std::cout << argv[1] << "\n";
     if (pcl::io::loadPCDFile<pcl::PointXYZRGB>(argv[1], *cloud) == -1) //* load the file
     {
         PCL_ERROR("Couldn't read file \n");
         return (-1);
     }
-    std::cout << "Loaded " << cloud->width * cloud->height << " data points from " << argv[1] << std::endl;
+    std::cout << "Loaded " << cloud->width * cloud->height << " data points" << std::endl;
 
-  
-    Visualizer<pcl::PointXYZRGB> vis;
+    
 
+    
 
-    Eigen::Matrix4f pose;
-    pose << 1, 0, 0, 0,
-            0, 1, 0, 0,
-            0, 0, 1, 0,
-            0, 0, 0, 1;
+    // Compute principal directions
+    Eigen::Vector4f pcaCentroid;
+    pcl::compute3DCentroid<pcl::PointXYZRGB>(*cloud, pcaCentroid);
+    Eigen::Matrix3f covariance;
+    pcl::computeCovarianceMatrixNormalized<pcl::PointXYZRGB>(*cloud, pcaCentroid, covariance);
+    Eigen::SelfAdjointEigenSolver<Eigen::Matrix3f> eigen_solver(covariance, Eigen::ComputeEigenvectors);
+    Eigen::Matrix3f eigenVectorsPCA = eigen_solver.eigenvectors();
+    eigenVectorsPCA.col(2) = eigenVectorsPCA.col(0).cross(eigenVectorsPCA.col(1)); /// This line is necessary for proper orientation in some cases. The numbers come out the same without it, but
+                                                                                   ///    the signs are different and the box doesn't get correctly oriented in some cases.
+    /* // Note that getting the eigenvectors can also be obtained via the PCL PCA interface with something like:
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloudPCAprojection (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PCA<pcl::PointXYZ> pca;
+    pca.setInputCloud(cloudSegmented);
+    pca.project(cloud, *cloudPCAprojection);
+    std::cerr << std::endl << "EigenVectors: " << pca.getEigenVectors() << std::endl;
+    std::cerr << std::endl << "EigenValues: " << pca.getEigenValues() << std::endl;
+    // In this case, pca.getEigenVectors() gives similar eigenVectors to eigenVectorsPCA.
+    */
 
-    vis.drawPointCloud(cloud, "firstPC", pose);
+    // Transform the original cloud to the origin where the principal components correspond to the axes.
+    Eigen::Matrix4f projectionTransform(Eigen::Matrix4f::Identity());
+    projectionTransform.block<3, 3>(0, 0) = eigenVectorsPCA.transpose();
+    projectionTransform.block<3, 1>(0, 3) = -1.f * (projectionTransform.block<3, 3>(0, 0) * pcaCentroid.head<3>());
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloudPointsProjected(new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::transformPointCloud(*cloud, *cloudPointsProjected, projectionTransform);
+    // Get the minimum and maximum points of the transformed cloud.
+    pcl::PointXYZRGB minPoint, maxPoint;
+    pcl::getMinMax3D(*cloudPointsProjected, minPoint, maxPoint);
+    const Eigen::Vector3f meanDiagonal = 0.5f * (maxPoint.getVector3fMap() + minPoint.getVector3fMap());
+    // Final transform
+    const Eigen::Quaternionf bboxQuaternion(eigenVectorsPCA);
+    const Eigen::Vector3f bboxTransform = eigenVectorsPCA * meanDiagonal + pcaCentroid.head<3>();
 
-    while(true){
-        vis.spinOnce();
+    // // This viewer has 4 windows, but is only showing images in one of them as written here.
+    pcl::visualization::PCLVisualizer *visu;
+    visu = new pcl::visualization::PCLVisualizer("PlyViewer");
+    int mesh_vp_1, mesh_vp_2, mesh_vp_3, mesh_vp_4;
+    visu->createViewPort(0.0, 0.5, 0.5, 1.0, mesh_vp_1);
+    visu->createViewPort(0.5, 0.5, 1.0, 1.0, mesh_vp_2);
+    visu->createViewPort(0.0, 0, 0.5, 0.5, mesh_vp_3);
+    visu->createViewPort(0.5, 0, 1.0, 0.5, mesh_vp_4);
+    visu->addPointCloud(cloud, ColorHandlerXYZ(cloud, 30, 144, 255), "bboxedCloud1", mesh_vp_1);
+    visu->addCube(bboxTransform, bboxQuaternion, maxPoint.x - minPoint.x, maxPoint.y - minPoint.y, maxPoint.z - minPoint.z, "bbox1", mesh_vp_2);
+    visu->addPointCloud(cloud, ColorHandlerXYZ(cloud, 30, 144, 255), "bboxedCloud3", mesh_vp_3);
+    visu->addCube(bboxTransform, bboxQuaternion, maxPoint.x - minPoint.x, maxPoint.y - minPoint.y, maxPoint.z - minPoint.z, "bbox2", mesh_vp_3);
+    visu->addPointCloud(cloud, ColorHandlerXYZ(cloud, 30, 144, 255), "bboxedCloud4", mesh_vp_4);
+    visu->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_OPACITY, 0.5 ,"bbox2");
+    visu->spinOnce(10,true);
+    //Visualizer<pcl::PointXYZRGB> vis;
+    //Eigen::Matrix4f pose;
+    //pose << 1, 0, 0, 0,
+    //    0, 1, 0, 0,
+    //    0, 0, 1, 0,
+    //    0, 0, 0, 1;
+    //vis.drawPointCloud(cloud, "firstPC", pose);
+    while (true)
+    {
+        //visu.spinOnce();
+        visu->spinOnce(10,true);
+
     }
 
     return 0;

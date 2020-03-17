3e8a111224c5361abc1f75a09a9c34359d795ce9
Ric92 <ricloplop@gmail.com>
2020-03-17 17:50:48 +0100

    Added segmentation example and viewer

---
+++ examples/viewer/viewer.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <pcl/io/io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/visualization/pcl_visualizer.h>
+
+using namespace std;
+
+void pointPickingEventOccurred (const pcl::visualization::PointPickingEvent& event, void* viewer_void)
+{
+  std::cout << "[INOF] Point picking event occurred." << std::endl;
+
+  float x, y, z;
+  if (event.getPointIndex () == -1)
+  {
+     return;
+  }
+  event.getPoint(x, y, z);
+  std::cout << "[INOF] Point coordinate ( " << x << ", " << y << ", " << z << ")" << std::endl;
+}
+    
+int main (int argc, char** argv)
+{
+    pcl::visualization::PCLVisualizer viewer("Cloud Viewer");
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr body (new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::io::loadPCDFile (argv[1], *body);
+    viewer.addPointCloud (body,"body");
+    viewer.registerPointPickingCallback (pointPickingEventOccurred, (void*)&viewer);
+    viewer.spin();
+    return 0;
+}

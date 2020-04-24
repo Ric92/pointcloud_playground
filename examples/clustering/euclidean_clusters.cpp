23dbf02f412c0716a36e499661f05d014070cd3d
marco montes <marrcogrova@gmail.com>
2020-04-24 09:11:34 +0200

    added euclidean clustering method

---
+++ examples/clustering/euclidean_clusters.cpp
@@ -0,0 +1,43 @@
+#include <pcl/io/pcd_io.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <iostream>
+
+int main(int argc, char** argv) {
+	// Object for storing the point cloud.
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+	// Read a PCD file from disk.
+	if (pcl::io::loadPCDFile<pcl::PointXYZ>(argv[1], *cloud) != 0){
+		return -1;
+	}
+
+    // Creating the KdTree object for the search method of the extraction
+    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+    tree->setInputCloud (cloud);
+
+    std::vector<pcl::PointIndices> cluster_indices;
+    pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
+    ec.setClusterTolerance (0.02); // 2cm
+    ec.setMinClusterSize (100);
+    ec.setMaxClusterSize (25000);
+    ec.setSearchMethod (tree);
+    ec.setInputCloud (cloud);
+    ec.extract (cluster_indices);
+
+    int j = 0;
+    for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
+    {
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZ>);
+        for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); ++pit)
+        cloud_cluster->points.push_back (cloud->points[*pit]); //*
+        cloud_cluster->width = cloud_cluster->points.size ();
+        cloud_cluster->height = 1;
+        cloud_cluster->is_dense = true;
+
+        std::cout << "Cluster " << j << " has " << cloud_cluster->points.size() << " points." << std::endl;
+		std::string fileName = "cluster" + boost::to_string(j) + ".pcd";
+		pcl::io::savePCDFileASCII(fileName, *cloud_cluster);
+        j++;
+    }
+    return -1;
+}
--- examples/filters/passthrough_filter.cpp
+++ examples/filters/passthrough_filter.cpp
@@ -2,14 +2,11 @@
 #include <pcl/filters/passthrough.h>
 #include <iostream>
 
-int
-main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
 	// Object for storing the point cloud.
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
 	// Read a PCD file from disk.
-	if (pcl::io::loadPCDFile<pcl::PointXYZ>(argv[1], *cloud) != 0)
-	{
+	if (pcl::io::loadPCDFile<pcl::PointXYZ>(argv[1], *cloud) != 0){
 		return -1;
 	}
 
@@ -27,20 +24,34 @@ main(int argc, char** argv)
         max_y = (cloud->points[i].y > max_y) ? cloud->points[i].y : max_y; 
         max_z = (cloud->points[i].z > max_z) ? cloud->points[i].z : max_z; 
     }
-
-    std::cout << "max x: " << max_x << " max y: " << max_y << " max z: " << max_z << std::endl; 
+    
+    // std::cout << "inc x: " << max_x - min_x << " inc y: " << max_y - min_y << " inc z: " << max_z - min_z << std::endl; 
+    // std::vector<float> incs = {max_x - min_x,max_x - min_x,max_x - min_x};
+    // int maxAxis = std::max_element(incs.begin(),incs.end()) - incs.begin();
+    
+    std::string filterAxis = "z";
+    float filterMaxLimit = max_z - (max_z - min_z)/2.0; 
 
     // Create the filtering object
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PassThrough<pcl::PointXYZ> pass;
     pass.setInputCloud (cloud);
+
+    // Cut in Z axis between 0.0 and max - inc/2
     pass.setFilterFieldName ("z");
-    pass.setFilterLimits (0.0, max_z - (max_z - min_z)/2.0);
+    pass.setFilterLimits (0.0, filterMaxLimit);
     //pass.setFilterLimitsNegative (true);
     pass.filter (*cloud_filtered);
 
 
     std::cout << "Cloud filtered has " << cloud_filtered->points.size() << " points." << std::endl;
+    
+    // Clean pointcloud
+    std::vector<int> indices;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_out (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::removeNaNFromPointCloud(*cloud_filtered, *cloud_out, indices);
+    std::cout << "size: " << cloud_out->points.size () << std::endl;
+
     std::string fileName = "cloud_filtered.pcd";
     pcl::io::savePCDFileASCII(fileName, *cloud_filtered);
     
